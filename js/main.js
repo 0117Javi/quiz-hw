8c7c80217a9228769e41f1c13cfa24f825a41890
jj espinoza <j.j.espinozagomez@gmail.com>
2021-05-18 20:57:02 -0500

    updated html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var startButton = document.getElementById('start-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-button')
+var answerButtonsElement = document.getElementById('answer-btn')
+var score = 0;
 
 let shuffleQuestions, currentQuestionIndex 
 
@@ -9,28 +10,29 @@ let shuffleQuestions, currentQuestionIndex
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('it worked!')
     startButton.classList.add('hide') 
     questionContainerElement.classList.remove('hide')
-    shuffleQuestions = question.sort(() => Math.random() - .5) 
-    currentQuestionIndex = 0
+    currentQuestionIndex = 0 
+    score = 0;
+    shuffleQuestions = questions.sort(() => Math.random() - .5) 
     questionContainerElement.classList.remove('hide') 
     nextQuestion()
 } 
 
-function nextQuestion() {
+function nextQuestion() { 
     showQuestion(shuffleQuestions[currentQuestionIndex])
 } 
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question 
+    answerButtonsElement.innerHTML=""
     question.answers.forEach(answer => {
         var button = document.createElement('button') 
         button.innerText = answer.text 
         button.classList.add('btn') 
-        if (answer.correct) {
+        // if (answer.correct) {
             button.dataset.correct = answer.correct
-        } 
+        // } 
         button.addEventListener('click', selectAnswer) 
         answerButtonsElement.appendChild(button)
     })
@@ -38,24 +40,35 @@ function showQuestion(question) {
 
 function resetState() {
     nextButton.classList.add('hide')
-    while(answerButtonElement.firstChild) {
-        answerButtonElement.removeChild 
-        (answerButtonElement.firstChild)
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild 
+        (answerButtonsElement.firstChild)
     }
 }
 
 function selectAnswer(e) {
     var selectedButton = e.target 
     var correct = selectedButton.dataset.correct 
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-}
+    setStatusClass(selectedButton, correct)
+    // Array.from(answerButtonsElement.children).forEach(button => {
+        
+    // })  
+    currentQuestionIndex++; 
+    setInterval(() => {
+        if( currentQuestionIndex < questions.length){
+            nextQuestion() 
+        } else {
+            // finish the game here 
+        } 
+    }, 2000);
+    
+    
+    
+} 
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if(correct) {
+    if(correct === "true") {
         element.classList.add('correct')
     } else {
         element.classList.add('wrong')
@@ -65,11 +78,13 @@ function setStatusClass(element, correct) {
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
-}
+} 
 
-var question = [
+
+
+var questions = [
     {
-        question: 'How so you write "hello world" in an alert box?', 
+        question: 'How do you write "hello world" in an alert box?', 
         answers:[
             { text: 'alert("hello world"):', correct: true},
             { text: 'msg("hello world"):', correct: false},
@@ -77,9 +92,8 @@ var question = [
             { text: 'Box("hello world"):', correct: false},
             
         ]
-    }
-]
-var question = [
+    },
+
     {
         question: 'How do you add a comment in JavaScript?', 
         answers:[
@@ -89,14 +103,13 @@ var question = [
             { text: '<This is a comment>', correct: false},
             
         ]
-    }
-]
-var question = [
+    },
+
     {
         question: 'JavaScript is the same as Java', 
         answers:[ 
-            { text: 'true', correct: true},
-            { text: 'false', correct: false},
+            { text: 'true', correct: false},
+            { text: 'false', correct: true},
             { text: 'sometimes', correct: false},
             { text: 'ish', correct: false},
         ]
